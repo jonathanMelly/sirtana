950e7c59d1a65f77c90adc570465257eb707e9fe
Jonathan Melly <jonathan.melly@gmail.com>
2020-03-11 13:51:12 +0100

    english translated comments

--- src/Sirtana/Drone.cs
+++ src/Sirtana/Drone.cs
@@ -1,63 +1,63 @@
-﻿//Auteur: JMY
+﻿//Author: JMY
 //Date: 15.02.2018
-//Lieu: ETML
-//Description: Version de base pour Sirtana
+//Place: ETML
+//Description: Base version of Sirtana
 using System;
 using System.IO;
 
 namespace Sirtana
 {
     /// <summary>
-    /// Drone parlant
+    /// Speaking drone
     /// </summary>
-    public class Drone //TODO : devenir observable
+    public class Drone //TODO : become observable
     {
         private Random random = new Random();
         private string[] answerData;
 
         /// <summary>
-        /// Constructeur basique d'un drone avec chargement des réponses dans un fichier
+        /// Base constructor with auto-loading of questions based on file
         /// </summary>
         public Drone()
         {
-            //Lecture du fichier
+            //Read file
             answerData = File.ReadAllLines("../../../data/answers.csv");
 
-            //Conversion du CSV en enlevant la première colonne qui numérote
+            //Convert CSV removing first column which is line number
             for (int i = 0; i < answerData.Length; i++)
             {
-                //Remplacement avec uniquement le message
+                //Replace with message only
                 answerData[i] = answerData[i].Split(';')[1];
             }
         }
 
         /// <summary>
-        /// Donne une réponse selon la question posée ou un message d'erreur si le format est erroné
+        /// Gives an answer regarding the question asked or an error message if the format is invalid
         /// </summary>
-        /// <param name="question"></param>
-        /// <returns>Une réponse ou un message d'erreur sur le format d'entrée</returns>
+        /// <param name="question">the text of the question</param>
+        /// <returns>An answer or an error message about the input</returns>
         public string Answer(string question)
         {
             string answer;
 
-            //Vérification du format
+            //Format check
             if (question.Contains("?"))
             {
-                //TODO : notifier les observateurs qu'une question valide a été posée
+                //TODO : Notify observers that a valid question has been asked
 
-                //On prend au hasard
-                //TODO : remplacer par la factory pour obtenir une réponse...
+                //Take random answer
+                //TODO : replae by a factory to get a suitable answer...
                 int randomNumber = random.Next(answerData.Length);
                 answer = answerData[randomNumber];
 
-                //TODO : notifier les observateurs qu'une réponse a été donnée
+                //TODO : Notify observers that an answer has been given
 
             }
-            //Mauvais format
+            //Bad format
             else
                 answer = "Veuiller poser une question en terminant avec un ?";
 
-            //Affichage de la réponse
+            //Returns result
             return answer;
         }
     }
