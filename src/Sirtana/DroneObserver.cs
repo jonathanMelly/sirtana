37c1de9071aeca3f2c75333ef451e3b4ce493eb9
Jonathan Melly <jonathan.melly@gmail.com>
2020-03-11 13:46:04 +0100

    translated comments in english

--- src/Sirtana/DroneObserver.cs
+++ src/Sirtana/DroneObserver.cs
@@ -1,24 +1,24 @@
-﻿//Auteur: JMY
+﻿//Author: JMY
 //Date: 15.02.2018
-//Lieu: ETML
-//Description: Version de base pour Sirtana
+//Place: ETML
+//Description: Base version of Sirtana
 namespace Sirtana
 {
     /// <summary>
-    /// Contrat pour un observateur de drone
+    /// Drone observer contract
     /// </summary>
     public interface DroneObserver
     {
         /// <summary>
-        /// Notification qu'une question valide a été posée
+        /// Notify when a question is asked
         /// </summary>
-        /// <param name="question">la question posée en texte</param>
+        /// <param name="question">question text</param>
         void QuestionAsked(string question);
 
         /// <summary>
-        /// Notification qu'une réponse a été donnée par le drone
+        /// Notify when an answer is given
         /// </summary>
-        /// <param name="answer">la réponse du drone en texte</param>
+        /// <param name="answer">text of the answer</param>
         void AnswerGiven(string answer);
     }
 }
